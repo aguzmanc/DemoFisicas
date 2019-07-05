6f8836ec15c51a725a5d6189fdede44bfc4c9751
Arnold Guzman <spacerockganimedes@gmail.com>
2019-07-05 00:05:55 -0400

    Experimentos: Gravedad, Velocidad y Tamanho

---
+++ Assets/Bala.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    [Range(0.1f, 1000f)]
+    public float velocidad=1f;
+
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = transform.forward * velocidad;
+    }
+}
